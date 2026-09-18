9d40f902ea72123460b09365e34a127350f0a5ce
slowwshy <arthurtoon13@gmail.com>
2026-09-18 17:02:19 -0300

    debugs

--- Python/interface.py
+++ Python/interface.py
@@ -4,6 +4,7 @@ import leitor
 
 app = QApplication(sys.argv)
 layout = QVBoxLayout()
+system = bool
 
 window = QWidget()
 window.setWindowTitle("Automa-es.py")
@@ -16,10 +17,15 @@ button.setFixedSize(250, 70)
 button.setStyleSheet("background-color: blue")
 layout.addWidget(button)
 
+def stop():
+    global system
+    system = False
+
 ButtonOff = QPushButton("Stop")
 ButtonOff.setFixedSize(250, 70)
 ButtonOff.setStyleSheet("background-color: red")
 layout.addWidget(ButtonOff)
+ButtonOff.clicked.connect(stop)
 
 def iniciar():
     
@@ -29,7 +35,7 @@ def iniciar():
     layout.addWidget(buttonConfirm)
     message_label = QLabel("Quando se conectar ao whatsapp, clique em confirmar para começar a automação.")
     layout.addWidget(message_label)
-    buttonConfirm.clicked.connect(leitor.leitor(buttonConfirm))
+    buttonConfirm.clicked.connect(lambda: leitor.leitor(system))
 
 button.clicked.connect(iniciar)
 
--- Python/leitor.py
+++ Python/leitor.py
@@ -2,9 +2,10 @@
 import time
 import re
 import main
+
 from playwright.sync_api import sync_playwright
 
-def leitor(buttonConfirm):
+def leitor(system):
     print("Iniciando automação...")
     with sync_playwright() as p:
 
@@ -20,25 +21,29 @@ def leitor(buttonConfirm):
 
         input("Entre no WhatsApp e abra a conversa. Pressione ENTER...")
 
+        while True:
+            try:
+                
+                elemento = pagina.get_by_text("não lidas", exact=False)
+                aba = pagina.get_by_role("tab", name="Não lidas")
+                numero = aba.get_by_text(re.compile(r"^\d+$"))
 
-    while True:
-        try:
-            elemento = pagina.get_by_text("não lidas", exact=False)
-            aba = pagina.get_by_role("tab", name="Não lidas")
-            numero = aba.get_by_text(re.compile(r"^\d+$"))
+                if numero.is_visible():
+                        time.sleep(1)
+                        print("TEM MENSAGEM NÃO LIDA!", elemento.count())
+                        main.processo(pagina)
+                else:
+                        time.sleep(1)
+                        print("Nenhuma mensagem não lida.")
 
-            if numero.is_visible():
-                    time.sleep(1)
-                    print("TEM MENSAGEM NÃO LIDA!", elemento.count())
-                    main.processo(pagina)
-            else:
-                    time.sleep(1)
-                    print("Nenhuma mensagem não lida.")
+                if system == False:
+                        system = True
+                        break
 
-
-        except Exception as erro:
-            print("[ERROR!!!]", erro)
-            break
+            except Exception as erro:
+                print("[ERROR!!!]", erro)
+                break
+               
 
 
             
