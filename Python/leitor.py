a3e48b91e45d3eed78d08c6dd09403192f7965f1
slowwshy <arthurtoon13@gmail.com>
2026-09-18 12:43:02 -0300

    refatora leitor e main

--- Python/leitor.py
+++ Python/leitor.py
@@ -1,13 +1,7 @@
-import os
+
 import time
-import subprocess
-import pyautogui 
-import pyscreeze
-import platform
-import easyocr
 import re
 import main
-import numpy as np
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
@@ -25,17 +19,7 @@ with sync_playwright() as p:
 
     while True:
         try:
-            elemento = pagina.locator("#unread-filter")
-
-            texto = elemento.inner_text().strip().splitlines()
-
-            if len(texto) > 1:
-                    time.sleep(1)
-                    print("🔔 TEM MENSAGEM NÃO LIDA!")
-                    main.main(texto)
-            else:
-                time.sleep(1)
-                print("Nenhuma mensagem não lida.")
+         
 
         except Exception as erro:
             print("[ERROR!!!]")
--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,10 @@
-import os
+
 import time
 import subprocess
-import pyautogui
-import pyscreeze
-import platform
-import easyocr
 import re
-from leitor import pagina, navegador
 
 
-def main(texto):
+
+def main(pagina):
        print("text")
    
